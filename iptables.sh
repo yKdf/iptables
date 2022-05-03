2f0a19d2c4255488cc66a0867a43d9fe266696e9
yKdf <104851075+yKdf@users.noreply.github.com>
2022-05-03 14:44:16 -0300

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -1,16 +1,16 @@
-iptables -t nat -A PREROUTING -d 10.1.0.6 -p tcp --dport 50000 -j DNAT --to-destination 10.8.0.2
-iptables -t nat -A PREROUTING -d 10.1.0.6 -p tcp --dport 50001 -j DNAT --to-destination 10.8.0.2
-iptables -t nat -A PREROUTING -d 10.1.0.6 -p tcp --dport 50002 -j DNAT --to-destination 10.8.0.2
-iptables -t nat -A PREROUTING -d 10.1.0.6 -p tcp --dport 50003 -j DNAT --to-destination 10.8.0.2
-iptables -t nat -A PREROUTING -d 10.1.0.6 -p tcp --dport 50004 -j DNAT --to-destination 10.8.0.2
-iptables -t nat -A PREROUTING -d 10.1.0.6 -p tcp --dport 50005 -j DNAT --to-destination 10.8.0.2
-iptables -t nat -A PREROUTING -d 10.1.0.6 -p tcp --dport 50006 -j DNAT --to-destination 10.8.0.2
-iptables -t nat -A PREROUTING -d 10.1.0.6 -p tcp --dport 50007 -j DNAT --to-destination 10.8.0.2
-iptables -t nat -A PREROUTING -d 10.1.0.6 -p tcp --dport 50008 -j DNAT --to-destination 10.8.0.2
-iptables -t nat -A PREROUTING -d 10.1.0.6 -p tcp --dport 50009 -j DNAT --to-destination 10.8.0.2
-iptables -t nat -A PREROUTING -d 10.1.0.6 -p tcp --dport 50010 -j DNAT --to-destination 10.8.0.2
+output iptables -t nat -A PREROUTING -d 10.1.0.6 -p tcp --dport 50000 -j DNAT --to-destination 10.8.0.2
+output iptables -t nat -A PREROUTING -d 10.1.0.6 -p tcp --dport 50001 -j DNAT --to-destination 10.8.0.2
+output iptables -t nat -A PREROUTING -d 10.1.0.6 -p tcp --dport 50002 -j DNAT --to-destination 10.8.0.2
+output iptables -t nat -A PREROUTING -d 10.1.0.6 -p tcp --dport 50003 -j DNAT --to-destination 10.8.0.2
+output iptables -t nat -A PREROUTING -d 10.1.0.6 -p tcp --dport 50004 -j DNAT --to-destination 10.8.0.2
+output iptables -t nat -A PREROUTING -d 10.1.0.6 -p tcp --dport 50005 -j DNAT --to-destination 10.8.0.2
+output iptables -t nat -A PREROUTING -d 10.1.0.6 -p tcp --dport 50006 -j DNAT --to-destination 10.8.0.2
+output iptables -t nat -A PREROUTING -d 10.1.0.6 -p tcp --dport 50007 -j DNAT --to-destination 10.8.0.2
+output iptables -t nat -A PREROUTING -d 10.1.0.6 -p tcp --dport 50008 -j DNAT --to-destination 10.8.0.2
+output iptables -t nat -A PREROUTING -d 10.1.0.6 -p tcp --dport 50009 -j DNAT --to-destination 10.8.0.2
+output iptables -t nat -A PREROUTING -d 10.1.0.6 -p tcp --dport 50010 -j DNAT --to-destination 10.8.0.2
 
 
-iptables -t nat -A PREROUTING -d 10.1.0.6 -p tcp --dport 2020 -j DNAT --to-destination 10.8.0.2
-iptables -t nat -A PREROUTING -d 10.1.0.6 -p tcp --dport 10003 -j DNAT --to-destination 10.8.0.2
-iptables -t nat -A POSTROUTING -s 10.8.0.2/32 -j SNAT --to-source 10.1.0.6
+output iptables -t nat -A PREROUTING -d 10.1.0.6 -p tcp --dport 2020 -j DNAT --to-destination 10.8.0.2
+output iptables -t nat -A PREROUTING -d 10.1.0.6 -p tcp --dport 10003 -j DNAT --to-destination 10.8.0.2
+output iptables -t nat -A POSTROUTING -s 10.8.0.2/32 -j SNAT --to-source 10.1.0.6
